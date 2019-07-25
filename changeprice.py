9fa8f61b77bdaae5662cad7d7e1187de00d74999
Sandeep Choudhary <35467518+sandeep7348@users.noreply.github.com>
2019-07-25 16:13:36 +0530

    Add files via upload

---
+++ changeprice.py
@@ -0,0 +1,32 @@
+from tkinter import*
+
+root2 = Tk()
+
+root2.geometry("200x120")
+root2.title("Menu")
+
+
+
+def enter1():
+	root2.destroy()
+	import restaurant_management_system
+
+
+def enter2():
+	root2.destroy()
+	import PriceFile
+
+
+labels = Label(root2, text="Bill")
+labels.grid(row=1, sticky=E)
+
+press_btn = Button(root2, text="Press Here", command= enter1)
+press_btn.grid(row=1, column=1)
+
+label21 = Label(root2, text="Change Price")
+label21.grid(row=2,sticky=E)
+
+press1_btn = Button(root2, text="Press Here", command= enter2)
+press1_btn.grid(row=2, column=1)
+
+root2.mainloop()
